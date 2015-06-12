0343676165976af94691e00d132d9cad01dea59f
Yusuke Ohashi <yusuke@junkpiano.me>
2015-06-13 00:33:06 +0900

    add unittest.

--- calc_test.go
+++ calc_test.go
@@ -1,5 +1,12 @@
 package main
 
-func testCalc() {
-  
+import (
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestCalc(t *testing.T) {
+  // assert equality
+  assert.Equal(t, add(2, 3), 5, "they should be equal")
+  assert.NotEqual(t, add(2, 3), 6, "they should not be equal")
 }
